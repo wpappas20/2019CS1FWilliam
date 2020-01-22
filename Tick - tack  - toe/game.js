4dca18209e76f198b1b9a947d82095580c7f4d27
MrArturtleNSCDS <agarcia@nscds.org>
2020-01-22 02:02:35 +0000

    Comments

--- Tick - tack  - toe/game.js
+++ Tick - tack  - toe/game.js
@@ -1,7 +1,15 @@
+//Like the file you showed me, you will need a variable to keep track of the clicks
+//This variable will increase with each click.
+
 $(".square").click(
     function(){
-        var color = $(this).css("background-color");
-        $('#display').css("background-color",color);
-        $('#display').text(color);
+        if(/* the number of clicks is odd*/){
+            $(this).css("background-color","red");
+        }
+        else{
+            $(this).css("background-color","blue");
+        }
     }
+
+    //increase your click counter
 );
