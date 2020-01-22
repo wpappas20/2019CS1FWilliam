866b33b9f3e4e9d815966c11cb7b987aab90ff96
wpappas20 <wpappas20@nscds.org>
2020-01-22 00:47:04 +0000

    js

--- Final Project - Worlds Hardest Game/game.js
+++
@@ -1,7 +0,0 @@
-$(".square").click(
-    function(){
-        var color = $(this).css("background-color");
-        $('#display').css("background-color",color);
-        $('#display').text(color);
-    }
-);
